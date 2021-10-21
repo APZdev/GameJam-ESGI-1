5ffa720162848060708359b253118b499ae40843
Antoine Pavy <antoine.pavy92@gmail.com>
2021-10-21 17:58:17 +0200

    Tweaked difficulty over time

--- GameJam-ESGI#1/Assets/Scripts/GameManager.cs
+++ GameJam-ESGI#1/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,7 +8,17 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Item[] itemsList;
     [SerializeField] private float spawnDelay = 1f;
     [SerializeField] private float spawnVariability = 0.5f;
+    [SerializeField] private float spawnMinimumDelay = 0.5f;
+    
+    public AnimationCurve SpeedIncrease;
+    public AnimationCurve EnemyIncrease;
+    
     private float _lastSpawn;
+    private float timeToSpawn;
+
+    private float timeElapsed;
+    private float startTime;
+    private float baseSpeed;
     
     public static GameManager Instance { get; private set; }
     private void Awake()
@@ -18,15 +29,24 @@ public class GameManager : MonoBehaviour
         else {
             Destroy(gameObject);
         }
+        startTime = Time.time;
+        baseSpeed = itemSpeed;
     }
 
-    private void Update() {
-        if (_lastSpawn > spawnDelay + Random.Range(-spawnVariability, spawnVariability)) {
+    private void Update()
+    {
+        timeElapsed = Time.time - startTime;
+        itemSpeed = baseSpeed * SpeedIncrease.Evaluate(timeElapsed);
+        if (_lastSpawn > timeToSpawn) {
             _lastSpawn = 0;
-            int index = Random.Range(0, itemsList.Length);
+            timeToSpawn = spawnMinimumDelay + (spawnDelay)/itemSpeed * Random.Range(0, spawnVariability);
+            float i = Random.Range(0f, 1f);
+            int index = i >= EnemyIncrease.Evaluate(timeElapsed) ? 0 : 1;
             Instantiate(itemsList[index], spawner.transform.position, Quaternion.identity);
         }
 
         _lastSpawn += Time.deltaTime;
+        
+        
     }
 }

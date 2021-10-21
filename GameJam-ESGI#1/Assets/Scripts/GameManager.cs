b485f770cc5a1209293fe7be033804f9a46a73bd
sikny <pamartqu@gmail.com>
2021-10-21 17:03:17 +0200

    instantiate prefabs every x seconds

--- GameJam-ESGI#1/Assets/Scripts/GameManager.cs
+++ GameJam-ESGI#1/Assets/Scripts/GameManager.cs
@@ -1,20 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public float itemSpeed = 5f;
+    [SerializeField] private Item[] itemsList;
+    [SerializeField] private float spawnDelay = 1f;
+    private float _lastSpawn;
     
     public static GameManager Instance { get; private set; }
     private void Awake()
     {
         if (Instance == null) {
             Instance = this;
-            //cameras = new List<ShakeTransform>();
         }
         else {
             Destroy(gameObject);
         }
+    }
+
+    private void Update() {
+        if (_lastSpawn > spawnDelay) {
+            _lastSpawn = 0;
+            int index = Random.Range(0, itemsList.Length);
+            Instantiate(itemsList[index]);
+        }
+
+        _lastSpawn += Time.deltaTime;
     }
 }
